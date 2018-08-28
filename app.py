7a41f4b40e7f6df343fc1ab3c988146e74201304
DenSev <den.sev@outlook.com>
2018-08-28 15:53:54 +0300

    added map object and map

--- app.py
+++ app.py
@@ -1,10 +1,92 @@
 import libtcodpy as libtcod
 
+#############################################
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 60
+#############################################
+MAP_WIDTH = 80
+MAP_HEIGHT = 50
+color_dark_wall = libtcod.Color(50, 50, 50)
+color_dark_ground = libtcod.Color(100, 100, 100)
+#############################################
+
+
+player_x = SCREEN_WIDTH / 2
+player_y = SCREEN_HEIGHT / 2
+
+
+class Object:
+
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        #move by the given amount, if the destination is not blocked
+        if not map[self.x + dx][self.y + dy].blocked:
+            self.x += dx
+            self.y += dy
+
+    def draw(self):
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+
+class Tile:
+    # a tile of the map and its properties
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        # by default, if a tile is blocked, it also blocks sight
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
+
+def make_map():
+    global map
+
+    # fill map with "unblocked" tiles
+    map = [[Tile(False)
+            for y in range(MAP_HEIGHT)]
+           for x in range(MAP_WIDTH)]
+
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+
+
+def render_all():
+    global color_light_wall
+    global color_light_ground
+
+    # go through all tiles, and set their background color
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+
+    # draw all objects in the list
+    for object in objects:
+        object.draw()
+
+    # blit the contents of con to the root console and present it
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
 
 def handle_keys():
     global player_x, player_y
 
-    #key = libtcod.console_check_for_keypress()  #real-time
+    # key = libtcod.console_check_for_keypress()  #real-time
     key = libtcod.console_wait_for_keypress(True)  # turn-based
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -16,34 +98,34 @@ def handle_keys():
 
     # movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        player_y -= 1
+        player.move(0, -1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        player_y += 1
+        player.move(0, 1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        player_x -= 1
+        player.move(-1, 0)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        player_x += 1
+        player.move(1, 0)
 
 
-#############################################
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
-LIMIT_FPS = 60
-player_x = SCREEN_WIDTH / 2
-player_y = SCREEN_HEIGHT / 2
+player = Object(player_x, player_y, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', libtcod.yellow)
+objects = [npc, player]
 
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 
+make_map()
+
 while not libtcod.console_is_window_closed():
 
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
-
+    render_all()
     libtcod.console_flush()
 
-    libtcod.console_put_char(0, player_x, player_y, ' ', libtcod.BKGND_NONE)
+    # erase all objects at their old locations before they move
+    for obj in objects:
+        obj.clear()
 
     # handle keys and exit game if needed
     shutdown = handle_keys()
